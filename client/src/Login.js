35ab5e5cd684bc98e670e8bd21b5b71b23d36cf3
Saran Jha <jhasaran9@gmail.com>
2021-05-24 23:38:01 -0700

    Modified to use components

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,118 +1,34 @@
 import React from "react";
-import { Redirect, useHistory } from "react-router-dom";
-import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 import {
   Grid,
-  Box,
-  Typography,
-  Button,
-  FormControl,
-  TextField,
-  Paper,
-  InputAdornment,
-  Link,
   Hidden
 } from "@material-ui/core";
-import { login } from "./store/utils/thunkCreators";
-import CustomButton from './components/CustomButton/CustomButton';
-import { ReactComponent as Logo } from "./static/bubble.svg";
+import Navigation from './components/Welcome/Navigation';
 import { useStyles } from "./Styles.js"
+import SideBanner from "./components/Welcome/SideBanner";
+import SignInForm from "./components/Welcome/SignInForm";
 
-
-const Login = (props) => {
+const Login = () => {
   const classes = useStyles();
   const history = useHistory();
-  const { user, login } = props;
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    const username = event.target.username.value;
-    const password = event.target.password.value;
-
-    await login({ username, password });
-  };
-
+  const header = "Don't have an account?";
+  const name = "Register";
   const onRouteChange = () => {
     history.push("/register")
   }
-
-  if (user.id) {
-    return <Redirect to="/home" />;
-  }
-
   return (
     <Grid container component="main" className={classes.root}>
       <Hidden xsDown>
-      <Grid item xs={false} sm={4} md={5}>
-        <Grid className={classes.image}>
-          <Grid container direction="column" className={classes.layer}>
-            <Logo />
-            <Typography variant="h4" className={classes.text}>Converse with anyone with any language</Typography>
-          </Grid>
-        </Grid>
-      </Grid>
+        <SideBanner />
       </Hidden>
-      <Grid item xs={12} sm={8} md={7} justify="center" component={Paper} elevation={6}>
-        <div className={classes.paper}>
-          <Grid container item justify="flex-end" alignItems="center">
-            <Typography color="textSecondary">Don't have an account?</Typography>
-            <CustomButton onRouteChange={onRouteChange}>Register</CustomButton>
-          </Grid>
-          <Grid container item >
-            <Box ml={15}><Typography variant="h4">Welcome Back!</Typography></Box>
-            <Grid container item justify="center">
-              <form onSubmit={handleLogin} className={classes.form}>
-                <FormControl margin="normal" required>
-                  <TextField
-                    className={classes.input}
-                    aria-label="username"
-                    label="Username"
-                    name="username"
-                    type="text"
-                  />
-                </FormControl>
-                <FormControl margin="normal" required>
-                  <TextField
-                    className={classes.input}
-                    label="Password"
-                    aria-label="password"
-                    type="password"
-                    name="password"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment component={Link} position="start">
-                          <Link href="#" color="primary">
-                            {"Forgot?"}
-                          </Link>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </FormControl>
-                <Button type="submit" variant="contained" size="large" color="primary" className={classes.submit}>
-                  Login
-                </Button>
-              </form>
-            </Grid>
-          </Grid>
-        </div>
+      <Grid item sm={8} md={6} justify="center">
+        <Navigation onRouteChange={onRouteChange} header={header} name={name} />
+        <SignInForm />
       </Grid>
     </Grid>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-  };
-};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    login: (credentials) => {
-      dispatch(login(credentials));
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default Login;
