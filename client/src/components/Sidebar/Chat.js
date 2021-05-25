48ef5e9186f3f90ebea1a3af43092cbc5835fc81
Saran Jha <jhasaran9@gmail.com>
2021-05-25 13:42:26 -0700

    Update chat

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box } from "@material-ui/core";
+import React, { Component } from "react";
+import { Box, Avatar } from "@material-ui/core";
 import { BadgeAvatar, ChatContent } from "../Sidebar";
 import { withStyles } from "@material-ui/core/styles";
 import { setActiveChat } from "../../store/activeConversation";
@@ -19,28 +19,55 @@ const styles = {
   },
 };
 
+const SmallAvatar = withStyles((theme) => ({
+  square: {
+    background: theme.palette.primary.main,
+    width: 'auto',
+    height: theme.spacing(3),
+    borderRadius: "3vh",
+    fontSize: theme.spacing(1.5),
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: "1vh"
+  }
+}))(Avatar);
+
 function Chat(props) {
   const { classes } = props;
   const otherUser = props.conversation.otherUser;
+  
+
   const handleClick = async (conversation) => {
     await props.setActiveChat(conversation.otherUser.username);
   };
 
-  return (
-    <Box
-      onClick={() => handleClick(props.conversation)}
-      className={classes.root}
-    >
-      <BadgeAvatar
-        photoUrl={otherUser.photoUrl}
-        username={otherUser.username}
-        online={otherUser.online}
-        sidebar={true}
-      />
-      <ChatContent conversation={props.conversation} />
-    </Box>
-  );
-}
+  const unreadMessages = () => {
+    const { user, conversation } = props;
+    const count = conversation.messages.reduce((unread, message) => {
+      if (message.senderId !== user.id && !message.read)
+        unread++;
+      return unread;
+    }, 0)
+    return count;
+  }
+
+  const unread = unreadMessages();
+    return (
+      <Box
+        onClick={() => handleClick(props.conversation)}
+        className={classes.root}
+      >
+        <BadgeAvatar
+          photoUrl={otherUser.photoUrl}
+          username={otherUser.username}
+          online={otherUser.online}
+          sidebar={true}
+        />
+        <ChatContent conversation={props.conversation} />
+        {unread > 0 ? <SmallAvatar variant="square">{unread}</SmallAvatar> : <Box></Box>}
+      </Box>
+    );
+  }
 
 const mapDispatchToProps = (dispatch) => {
   return {
