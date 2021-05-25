5786437c76c36db28e078aca0ff994dd0c84b1ee
Saran Jha <jhasaran9@gmail.com>
2021-05-25 12:23:08 -0700

    Create Input1.js

---
+++ client/src/components/ActiveChat/Input1.js
@@ -0,0 +1,72 @@
+import React, { useState} from "react";
+import { FormControl, FilledInput } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import { postMessage } from "../../store/utils/thunkCreators";
+import { useStyles } from "../../Styles";
+
+const styles = {
+    root: {
+      justifySelf: "flex-end",
+      marginTop: 15,
+    },
+    input: {
+      height: 70,
+      backgroundColor: "#F4F6FA",
+      borderRadius: 8,
+      marginBottom: 20,
+    },
+  };
+
+function Input(props){
+    const [text, setText] = useState("");
+    const classes = useStyles();
+    
+    const handleChange = (event) => {
+        console.log("Changing text");
+        setText(event.target.value);
+    }
+
+    const handleSubmit = async (event) => {
+        const reqBody = {
+            text: event.target.text.value,
+            recipientId: props.otherUser.id,
+            conversationId: props.conversationId,
+            sender: props.conversationId ? null : props.user,
+          };
+          await props.postMessage(reqBody);
+          setText("");
+    }
+
+    return(
+        <form className={classes.root} onSubmit={handleSubmit}>
+        <FormControl fullWidth hiddenLabel>
+          <FilledInput
+            classes={{ root: classes.input }}
+            disableUnderline
+            placeholder="Type something..."
+            value={text}
+            name="text"
+            onChange={handleChange}
+          />
+        </FormControl>
+      </form>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {
+      user: state.user,
+      conversations: state.conversations,
+    };
+  };
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      postMessage: (message) => {
+        dispatch(postMessage(message));
+      },
+    };
+  };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Input);
