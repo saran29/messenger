ab1553359327837eb860a3ce48d8708651540ca5
Saran Jha <jhasaran9@gmail.com>
2021-05-25 13:37:19 -0700

    Revert "Revert "Merge branch 'read_status' into fe_optimization""
    This reverts commit 0b334d9285843171cdebb1b3734c8ee93ef3e8ea.

---
+++ server/routes/api/read.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const { Conversation, Message } = require("../../db/models");
+const { Op } = require("sequelize");
+
+// expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
+router.put("/", async (req, res, next) => {
+  try {
+    if (!req.user) {
+      return res.sendStatus(401);
+    }
+      const { recipientId, senderId } = req.body;
+      if(recipientId){
+    // if we don't have conversation id, find a conversation to make sure it doesn't already exist
+    let conversation = await Conversation.findConversation(
+      senderId,
+      recipientId
+    );
+
+    const messages = await Message.update({read : '1'},{
+        where: {
+            [Op.and]: [
+              { conversationId: conversation.id },
+              { read: '0' }
+            ]
+          },
+    })
+    res.sendStatus(200);
+    }
+    //const senderId = req.user.id;
+    
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
