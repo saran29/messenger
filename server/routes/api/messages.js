49c3c355a75d1f103d20a81d35308caa42c6a9fc
Saran Jha <jhasaran9@gmail.com>
2021-05-26 22:13:32 -0700

    Update messages.js

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res, next) => {
       return res.sendStatus(401);
     }
     const senderId = req.user.id;
-    const { recipientId, text, sender } = req.body;
+    const { recipientId, conversationId, text, sender } = req.body;
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
